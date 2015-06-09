e61380728779dcfbf12282e800265f5feff8475f
Antonio <ajosegmartinez@gmail.com>
2015-06-09 17:39:04 -0400

    Express-partials

--- app.js
+++ app.js
@@ -6,15 +6,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-
+var partials = require('express-partials');
 var app = express();
 app.use(favicon(__dirname + '/public/favicon.ico'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(partials());
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
